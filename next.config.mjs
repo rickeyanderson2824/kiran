2b210113c2891ead72e43596a779174580275e6a
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-11-24 13:47:52 +0530

    update Next.js configuration to enable experimental app directory and set output to standalone for Vercel compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Ensures the use of the latest App Router (if you're using it)
+    experimental: {
+      appDir: true, // Enables the `/app` directory if you're using the new App Router
+    },
+    
+    // Output configuration
+    output: 'standalone', // Ensures compatibility with Vercel deployments
+  };
+  
+  export default nextConfig;
+  
